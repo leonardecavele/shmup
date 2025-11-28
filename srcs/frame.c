3f488fc5d0ce1bc66c80d23dfbdd4424f0dc758e
leonardecavele <leonard.decavele@gmail.com>
2025-11-29 00:40:23 +0100

    new: functions to keep the framerate at 60 fps

---
+++ srcs/frame.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   frame.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldecavel <ldecavel@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/29 00:16:18 by ldecavel          #+#    #+#             */
+/*   Updated: 2025/11/29 00:37:44 by ldecavel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "frame.h"
+
+extern void	sleep_remaining(double time)
+{
+    struct timespec	remaining;
+
+    if (time < 1)
+        return ;
+    remaining.tv_sec = time;
+    remaining.tv_nsec = (time - remaining.tv_sec) * 1e9;
+    nanosleep(&remaining, NULL);
+}
+
+extern double	get_time(void)
+{
+	struct timespec tp;
+
+	clock_gettime(CLOCK_MONOTONIC, &tp);
+	return (tp.tv_sec + tp.tv_nsec / 1e9);
+}
