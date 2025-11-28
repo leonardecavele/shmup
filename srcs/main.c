f6e9eebeba3eb41a9160c746dc86541a61ec7553
leonardecavele <leonard.decavele@gmail.com>
2025-11-28 22:00:17 +0100

    new: main loop structure

---
+++ srcs/main.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldecavel <ldecavel@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/28 21:47:32 by ldecavel          #+#    #+#             */
+/*   Updated: 2025/11/28 21:59:23 by ldecavel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#define TARGET_FRAME_TIME 60
+
+#include <stdbool.h>
+
+int	main(void)
+{
+	bool	playing = true;
+
+	while (playing)
+	{
+		// frame_start = get_time();
+		//
+		// get_input();
+		// update_game();
+		// render();
+		//
+		// frame_end = get_time();
+		//
+		// frame_time = frame_end - frame_start;
+
+		// if (frame_time < TARGET_FRAME_TIME)
+		//		wait_remaining_time();
+	}
+	return (0);
+}
