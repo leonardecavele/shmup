f506b09ad6a5b9dc2926a6e9c2169c88d5ab1ee5
leonardecavele <leonard.decavele@gmail.com>
2025-11-29 00:40:57 +0100

    edit: basic loop at 60 fps

--- srcs/main.c
+++ srcs/main.c
@@ -6,32 +6,30 @@
 /*   By: ldecavel <ldecavel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/28 21:47:32 by ldecavel          #+#    #+#             */
-/*   Updated: 2025/11/28 21:59:23 by ldecavel         ###   ########.fr       */
+/*   Updated: 2025/11/29 00:19:18 by ldecavel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#define TARGET_FRAME_TIME 60
-
+#include "frame.h"
 #include <stdbool.h>
 
 int	main(void)
 {
 	bool	playing = true;
+	float	frame_start, frame_time, frame_end;
 
 	while (playing)
 	{
-		// frame_start = get_time();
+		frame_start = get_time();
 		//
 		// get_input();
 		// update_game();
 		// render();
 		//
-		// frame_end = get_time();
-		//
-		// frame_time = frame_end - frame_start;
-
-		// if (frame_time < TARGET_FRAME_TIME)
-		//		wait_remaining_time();
+		frame_end = get_time();
+		frame_time = frame_end - frame_start;
+		if (frame_time < TARGET_FRAME_TIME)
+			sleep_remaining(TARGET_FRAME_TIME - frame_time);
 	}
 	return (0);
 }
