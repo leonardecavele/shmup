0746f29c0e6b735963631e0920ede34f3b51736f
leonardecavele <leonard.decavele@gmail.com>
2025-11-28 22:13:49 +0100

    new: draft of the render method

---
+++ srcs/render.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   render.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldecavel <ldecavel@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/28 22:08:21 by ldecavel          #+#    #+#             */
+/*   Updated: 2025/11/28 22:09:25 by ldecavel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// to simulate layers we can draw in a specific order
+
+void	render(void)
+{
+	// draw map
+	// draw character
+	// draw enemies
+	// refresh();
+}
