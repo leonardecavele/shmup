d90a2b1667a3301c8e6b71d9f97a1150b4ff2506
Leonard Decavele <ldecavel@z2r9p8.42lyon.fr>
2025-11-29 11:00:11 +0100

    new: draft of the main game function

---
+++ srcs/game.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   game.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldecavel <ldecavel@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/29 08:56:10 by ldecavel          #+#    #+#             */
+/*   Updated: 2025/11/29 10:20:10 by ldecavel         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "game.h"
+#include "shmup.h"
+
+extern int	update_game(int c)		// if returns not 0, game is ended
+{
+	(void)c;
+	return (0);
+}
