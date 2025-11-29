6376a74d4aaff913bbc3a2d82c17d9db66b001f7
Leonard Decavele <ldecavel@z4r6p3.42lyon.fr>
2025-11-29 16:19:25 +0100

    edit: changed min map size to run tests

--- includes/parser.h
+++ includes/parser.h
@@ -6,7 +6,7 @@
 /*   By: ldecavel <ldecavel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/29 11:37:54 by ldecavel          #+#    #+#             */
-/*   Updated: 2025/11/29 13:48:07 by ldecavel         ###   ########lyon.fr   */
+/*   Updated: 2025/11/29 16:00:48 by ldecavel         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@
 # define FD_MAX				1024
 # define INFO_LINE_SIZE		16
 
-# define MIN_BOARD_HEIGHT	30
-# define MIN_BOARD_WIDTH	30
+# define MIN_BOARD_HEIGHT	10
+# define MIN_BOARD_WIDTH	10
 
 bool	parse(t_game *game, int fd);
 
