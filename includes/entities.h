9a3d594f1708f7b672dff60c0ca5a65d614005d1
Leonard Decavele <ldecavel@z4r6p3.42lyon.fr>
2025-11-29 14:31:02 +0100

    new: entities

---
+++ includes/entities.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   entities.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldecavel <ldecavel@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/29 13:59:37 by ldecavel          #+#    #+#             */
+/*   Updated: 2025/11/29 14:29:09 by ldecavel         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ENTITIES_H
+# define ENTITIES_H
+
+# define HERO	@
+# define ENEMY1	1
+# define ENEMY1	2
+# define ENEMY1	3
+# define BOSS	&
+# define WALL	#
+# define EMPTY	. 
+
+#endif
