7653c870b35272fcae96ef466655cdd156af1101
Leonard Decavele <ldecavel@z2r9p8.42lyon.fr>
2025-11-29 10:53:13 +0100

    edit: fps now change every seconds

--- srcs/frame.c
+++ srcs/frame.c
@@ -6,7 +6,7 @@
 /*   By: ldecavel <ldecavel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/29 00:16:18 by ldecavel          #+#    #+#             */
-/*   Updated: 2025/11/29 01:39:44 by ldecavel         ###   ########.fr       */
+/*   Updated: 2025/11/29 10:30:16 by ldecavel         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,17 +32,18 @@ extern double	get_time(void)
 	return (tp.tv_sec + tp.tv_nsec / 1e9);
 }
 
-extern void	display_fps(double time)
+extern void	display_fps(double time) 
 {
+	static double	fps = 0;
 	static double	added_time = 0.0;
 	static int		added_frame = 0;
 
 	added_frame++;
 	added_time += time;
+	mvprintw(0, 0, "%6.1f fps", fps);
 	if (added_time >= 1.0)
 	{
-		mvprintw(0, 0, "%6.1f fps", added_frame / added_time);
-		refresh();
+		fps = added_frame / added_time;
 		added_time = 0.0;
 		added_frame = 0;
 	}
