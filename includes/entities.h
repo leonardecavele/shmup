60ec573f035716b4aa185492d3f4a5ff78a67e8b
Leonard Decavele <ldecavel@z4r6p3.42lyon.fr>
2025-11-29 15:38:00 +0100

    new: added 'GROUND'

--- includes/entities.h
+++ includes/entities.h
@@ -6,19 +6,20 @@
 /*   By: ldecavel <ldecavel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/29 13:59:37 by ldecavel          #+#    #+#             */
-/*   Updated: 2025/11/29 14:29:09 by ldecavel         ###   ########lyon.fr   */
+/*   Updated: 2025/11/29 15:30:30 by ldecavel         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ENTITIES_H
 # define ENTITIES_H
 
-# define HERO	@
-# define ENEMY1	1
-# define ENEMY1	2
-# define ENEMY1	3
-# define BOSS	&
-# define WALL	#
-# define EMPTY	. 
+# define HERO	'@'
+# define ENEMY1	'1'
+# define ENEMY1	'2'
+# define ENEMY1	'3'
+# define BOSS	'&'
+# define WALL	'#'
+# define EMPTY	'.' 
+# define GROUND '^'
 
 #endif
