4a5eb599cc3f4807e8a2d74d137bd0281682e4fb
Leonard Decavele <ldecavel@z2r9p8.42lyon.fr>
2025-11-29 10:53:42 +0100

    new: handles signals and window resize

--- srcs/main.c
+++ srcs/main.c
@@ -6,43 +6,81 @@
 /*   By: ldecavel <ldecavel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/28 21:47:32 by ldecavel          #+#    #+#             */
-/*   Updated: 2025/11/29 01:45:57 by ldecavel         ###   ########.fr       */
+/*   Updated: 2025/11/29 10:51:18 by ldecavel         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shmup.h"
 #include "frame.h"
+#include "game.h"
+#include "render.h"
+
+static volatile sig_atomic_t	to_resize = true;
+
+static void	panic(int sig)
+{
+	(void)sig;
+	endwin();
+	exit(0);
+}
+
+static void	handle_winch(int sig)
+{
+	(void)sig;
+	to_resize = true;
+}
+
+static void	handle_signal(void)
+{
+	signal(SIGINT, panic);
+	signal(SIGTERM, panic);
+	signal(SIGQUIT, panic);
+	signal(SIGWINCH, handle_winch);
+}
 
 int	main(void)
 {
-	bool	playing = true;
+	bool	correct_size, playing = true;
 	double	frame_start, frame_time, frame_end, fps;
+	int		c;
 
 	frame_start = frame_time = frame_end = fps = 0.0;
 
-	initscr();					// init screen
-	cbreak();					// keys instantly works
-	noecho();					// typed char are not printed
-	nodelay(stdscr, true);		// getch returns ERR if no char
-	curs_set(0);				// hide cursor
+	handle_signal();				// to properly exit
+	initscr();						// init screen
+	cbreak();						// keys instantly works
+	noecho();						// typed char are not printed
+	nodelay(stdscr, true);			// getch returns ERR if no char
+	curs_set(0);					// hide cursor
 
 	while (playing)
 	{
 		frame_start = get_time();
 
-		//
-		// get_input();
-		// update_game();
-		// render();
-		//
+		if (to_resize)
+		{
+			correct_size = resize();
+			if (correct_size)
+				to_resize = false;
+		}
+		if (correct_size == true)
+		{
+			c = getch();
+			if (update_game(c))
+				playing = false;
+
+			clear();
+			mvprintw(5, 10, "running");
+			// render();
+		}
 
 		frame_end = get_time();
 		frame_time = frame_end - frame_start;
 		if (frame_time < TARGET_FRAME_TIME)
 			sleep_remaining(TARGET_FRAME_TIME - frame_time);
-
 		display_fps(get_time() - frame_start);
+		refresh();
 	}
-	endwin();					// end screen
+	endwin();
 	return (0);
 }
--- srcs/render.c
+++ srcs/render.c
@@ -6,13 +6,31 @@
 /*   By: ldecavel <ldecavel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/28 22:08:21 by ldecavel          #+#    #+#             */
-/*   Updated: 2025/11/29 00:53:35 by ldecavel         ###   ########.fr       */
+/*   Updated: 2025/11/29 10:51:03 by ldecavel         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "render.h"
 #include "shmup.h"
 
 // to simulate layers we can draw in a specific order
+
+extern bool	resize(void)
+{
+	const char	message[] = "Please get a larger window.";
+	int			height, width;
+	
+	height = width = 0;
+
+	endwin();
+	refresh();
+	clear();
+	getmaxyx(stdscr, height, width);
+	if (height >= MIN_HEIGHT && width >= MIN_WIDTH)
+		return (true);
+	mvprintw(height / 2, (width / 2) - (strlen(message) / 2), message);
+	return (false);
+}
 
 void	render(void)
 {
