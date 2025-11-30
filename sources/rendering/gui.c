bfe15feb950b87dd9721c0311cb846ca45172e44
aB-temps <187414947+aB-temps@users.noreply.github.com>
2025-11-30 18:21:07 +0100

    ✨ feat(gui): minor changes

--- sources/rendering/gui.c
+++ sources/rendering/gui.c
@@ -6,7 +6,7 @@
 /*   By: abetemps <abetemps@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/30 16:26:36 by abetemps          #+#    #+#             */
-/*   Updated: 2025/11/30 17:50:23 by ldecavel         ###   ########.fr       */
+/*   Updated: 2025/11/30 18:07:35 by abetemps         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ static void display_hp(int hp, int x)
 		attron(COLOR_PAIR(1));
 		for (int i = 0; i < HERO_HP; ++i)
 		{
-			mvprintw(0, (x >> 1) - 4 + i, "%c", (i < hp ? HERO : ' '));
+			mvprintw(0, (x >> 2) + 3 + i, "%c", (i < hp ? HERO : ' '));
 		}
 		attroff(COLOR_PAIR(1));
 	}
@@ -30,7 +30,7 @@ static void display_hp(int hp, int x)
 		attron(COLOR_PAIR(3));
 		for (int i = 0; i < HERO_HP; ++i)
 		{
-			mvprintw(0, (x >> 1) - 4 + i, "%c", (i < hp ? HERO : ' '));
+			mvprintw(0, (x >> 2) + 3 + i, "%c", (i < hp ? HERO : ' '));
 		}
 		attroff(COLOR_PAIR(3));
 	}
@@ -39,7 +39,7 @@ static void display_hp(int hp, int x)
 		attron(COLOR_PAIR(2));
 		for (int i = 0; i < HERO_HP; ++i)
 		{
-			mvprintw(0, (x >> 1) - 4 + i, "%c", (i < hp ? HERO : ' '));
+			mvprintw(0, (x >> 2) + 3 + i, "%c", (i < hp ? HERO : ' '));
 		}
 		attroff(COLOR_PAIR(2));
 	}
@@ -50,19 +50,19 @@ static void display_score(int score, int best, int x)
 	if (score == best)
 	{
 		attron(COLOR_PAIR(1));
-		mvprintw(0, (x >> 1) + 1, "%d points", score);
+		mvprintw(0, (x >> 1) - 1, "%d %c", score, KEY);
 		attroff(COLOR_PAIR(1));
 	}
 	else if (score > (best >> 1))
 	{
 		attron(COLOR_PAIR(3));
-		mvprintw(0, (x >> 1) + 1, "%d points", score);
+		mvprintw(0, (x >> 1) - 1, "%d %c", score, KEY);
 		attroff(COLOR_PAIR(3));
 	}
 	else
 	{
 		attron(COLOR_PAIR(2));
-		mvprintw(0, (x >> 1) + 1, "%d points", score);
+		mvprintw(0, (x >> 1) - 1, "%d %c", score, KEY);
 		attroff(COLOR_PAIR(2));
 	}
 }
@@ -74,7 +74,7 @@ static void display_ammo(int ammo, int x)
 		attron(COLOR_PAIR(1));
 		for (int i = 0; i < MAX_PROJECTILES; ++i)
 		{
-			mvprintw(0, (x >> 1) + (x >> 2) - (MAX_PROJECTILES >> 1) + i, "%c", (i < ammo ? 'Z' : ' '));
+			mvprintw(0, (x >> 1) + (x >> 2) - (MAX_PROJECTILES) + i, "%c", (i < ammo ? 'Z' : ' '));
 		}
 		attroff(COLOR_PAIR(1));
 	}
@@ -83,7 +83,7 @@ static void display_ammo(int ammo, int x)
 		attron(COLOR_PAIR(3));
 		for (int i = 0; i < MAX_PROJECTILES; ++i)
 		{
-			mvprintw(0, (x >> 1) + (x >> 2) - (MAX_PROJECTILES >> 1) + i, "%c", (i < ammo ? 'Z' : ' '));
+			mvprintw(0, (x >> 1) + (x >> 2) - (MAX_PROJECTILES) + i, "%c", (i < ammo ? 'Z' : ' '));
 		}
 		attroff(COLOR_PAIR(3));
 	}
@@ -92,7 +92,7 @@ static void display_ammo(int ammo, int x)
 		attron(COLOR_PAIR(2));
 		for (int i = 0; i < MAX_PROJECTILES; ++i)
 		{
-			mvprintw(0, (x >> 1) + (x >> 2) - (MAX_PROJECTILES >> 1) + i, "%c", (i < ammo ? 'Z' : ' '));
+			mvprintw(0, (x >> 1) + (x >> 2) - (MAX_PROJECTILES) + i, "%c", (i < ammo ? 'Z' : ' '));
 		}
 		attroff(COLOR_PAIR(2));
 	}
